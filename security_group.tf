e4ceeffc890acd4503a108f925498feb5e81b955
Frédéric Pillet <frederic@pillet.io>
2025-03-17 11:53:57 +0100

    Create security_group.tf

---
+++ security_group.tf
@@ -0,0 +1,21 @@
+resource "aws_security_group" "demo_sg" {
+  vpc_id = aws_vpc.demo_vpc.id
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "demo_sg"
+  }
+}
